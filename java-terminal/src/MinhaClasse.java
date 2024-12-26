6047ee5d353b27b0e32d33b5dbcba1c1a193d346
Amanda Caroline Perez Pinto <amandacperezp@gmail.com>
2024-12-26 13:15:00 -0300

    terminal parte 2 teste

---
+++ java-terminal/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+    public MinhaClasse() {
+    }
+ 
+    public static void main(String[] args) throws Exception {
+       System.out.println("Oi Turma, fui executado pelo terminal");
+    }
+ }
+ 
